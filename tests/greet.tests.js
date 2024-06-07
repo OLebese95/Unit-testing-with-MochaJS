a176be341ab35f7db49b6098836dc0dbb839cd86
OLebse@95 <fentselebese@gmail.com>
2024-06-07 10:29:04 +0200

    added

--- tests/greet.tests.js
+++ tests/greet.tests.js
@@ -4,7 +4,17 @@ describe('Greet function' , function(){
         assert.equal(greet("Thato"), "Hello, Thato");
     });
 
+    it("should handle an empty string" , function(){
+        assert.equal(greet(""), "Hello, ");
+    });
+
+    it("should handle names with spaces" , function(){
+        assert.equal(greet("John Doe"), "Hello, John Wick");
+    });
+
 });
 
 console.log(greet("Ofentse"));
 console.log(greet("Thato"));
+console.log(greet(""));
+console.log(greet("John Wick"));
