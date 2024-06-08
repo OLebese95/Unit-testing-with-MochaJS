7a2e62f4b231c3595be3509fb1e42cd868236747
OLebse@95 <fentselebese@gmail.com>
2024-06-08 09:39:28 +0200

    make these ones

--- functions/countAllPaarl.js
+++ functions/countAllPaarl.js
@@ -1,3 +1,7 @@
+var regNumbers = 'CL 900, CJ 678 543, CA 34567, CJ 67890, CN 7864';
+var regNumbersForPaarl = allPaarl(regNumbers);
+
+
 function allPaarl(regNumbersString) {
     var regNumbersArray = regNumbersString.split(', ');
     var regNumbersForPaarl = [];
--- tests/countAllPaarl.tests.js
+++ tests/countAllPaarl.tests.js
@@ -1,15 +1,11 @@
 describe('countAllPaarl function' , function(){
     it("it should filter data from a string and return Paarl registration numbers", function() {
-        var regNumbers = 'CL 900, CJ 678 543, CA 34567, CJ 67890, CN 7864';
-        var regNumbersForPaarl = allPaarl(regNumbers);
         assert.equal(2, regNumbersForPaarl.length, 'There are 2 Paarl reg numbers.');
         assert.equal('CJ 678 543', regNumbersForPaarl[0]);
         assert.equal('CJ 67890', regNumbersForPaarl[1]);
     });
 
     it("it should return all registration numbers when all are from Paarl", function() {
-        var regNumbers = 'CJ 12345, CJ 67890, CJ 54321';
-        var regNumbersForPaarl = allPaarl(regNumbers);
         assert.equal(3, regNumbersForPaarl.length, 'There are 3 Paarl reg numbers.');
         assert.equal('CJ 12345', regNumbersForPaarl[0]);
         assert.equal('CJ 67890', regNumbersForPaarl[1]);
