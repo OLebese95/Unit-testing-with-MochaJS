670a781fe040709da90fb60c7362a77c9c161090
OLebse@95 <fentselebese@gmail.com>
2024-06-08 09:54:08 +0200

    been too lonng

--- tests/countAllPaarl.tests.js
+++ tests/countAllPaarl.tests.js
@@ -1,15 +1,11 @@
 describe('countAllPaarl function' , function(){
     it("it should filter data from a string and return Paarl registration numbers", function() {
-        assert.equal(2, regNumbersForPaarl.length, 'There are 2 Paarl reg numbers.');
         assert.equal('CJ 678 543', regNumbersForPaarl[0]);
         assert.equal('CJ 67890', regNumbersForPaarl[1]);
     });
 
-    it("it should return all registration numbers when all are from Paarl", function() {
-        assert.equal(3, regNumbersForPaarl.length, 'There are 3 Paarl reg numbers.');
-        assert.equal('CJ 12345', regNumbersForPaarl[0]);
-        assert.equal('CJ 67890', regNumbersForPaarl[1]);
-        assert.equal('CJ 54321', regNumbersForPaarl[2]);
+    it("it should count how many registration numbers are from Paarl in a string", function() {
+        assert.equal(2, regNumbersForPaarl.length, 'There are 2 Paarl reg numbers.');
     });
 });
 
