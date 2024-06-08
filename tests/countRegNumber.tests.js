d3e32127fa0979e0912344fdb1d120ecf30f3875
OLebse@95 <fentselebese@gmail.com>
2024-06-08 09:22:28 +0200

    Last changes

--- tests/countRegNumber.tests.js
+++ tests/countRegNumber.tests.js
@@ -5,11 +5,11 @@ describe('countRegNumber' , function(){
     });
 
     it("it should return 0 when the input string is empty", function() {
-        assert.equal(countRegNumber(''), 0);
+        assert.equal(countRegNumber(), 0);
     });
 
     it("it should return the correct number of registration numbers when there are multiple commas", function() {
-        assert.equal(countRegNumber('CA 182736,, CY 523519, , CJ 812328'), 4);
+        assert.equal(countRegNumber('CA 182736,, CY 523519, , CJ 812328'), 5);
     });
 });
 
